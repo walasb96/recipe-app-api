2319f8c48280f6c60e30a4eede832a82e92b1089
Bendra Walid <walid.bendra@outlook.fr>
2024-08-25 14:07:20 +0200

    update on files and add some test with calc.py

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Recipe API project
 
 
 sudo docker compose run --rm app sh -c "django-admin startproject app ."
+
+use unittest.mock
+    * MagicMock/Mock Replace real object
+    * patch Overrides code for tests
+
+* PostgreSQL:
+    * Popular open source DB
+    * Integrates ell with Django
+* Docker Compose:
+    * Dfines with project (reusable)
+    * Persistent data using volumes
+    * Handles network configuration
+    * Environment variables configuration
---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module."""
+    
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        rest = calc.add(5, 6)
+        
+        self.assertEqual(rest, 11)
+        
+    def test_subtract_numbers(self):
+        """Test subtracting numbers together."""
+        rest = calc.subtract(10, 15)
+        
+        self.assertEqual(rest, -5)
