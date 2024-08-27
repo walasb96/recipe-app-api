ea93d9e705f469f5c4df1954085baad3bea0d1d7
Bendra Walid <walid.bendra@outlook.fr>
2024-08-27 20:59:52 +0200

    fix issue django can not connect to db and fix linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,20 +3,26 @@ Django command to wait for the database to be available
 """
 
 # from typing import Any
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
-
-# class Command(BaseCommand):
-#     """Django command to wait for the database."""
-    
-#     def handle(self, *args: Any, **options: Any) -> str | None:
-#         pass
-#         # return super().handle(*args, **options)
-        
 class Command(BaseCommand):
     """Django command to wait for the database."""
-    
+
     def handle(self, *args, **options):
-        pass
-        # return super().handle(*args, **options)
+        """Entry point for command."""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+                break
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Database not ready. \
+                    Retrying in 1 second...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database avaliable!'))
---
+++ app/verif.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASE_NAME = os.getenv('DB_NAME')
+print(DATABASE_NAME)
+host = os.getenv('DB_HOST')
+print(host)
+user = os.getenv('DB_USER')
+print(user)
+password = os.getenv('DB_PASS')
+print(password)
+pwd = os.getenv('DB_PASSWORD')
+print(pwd)
